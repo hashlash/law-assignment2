4116e4d3735b3d82a74b8c7a6dc0766673a25a30
hashlash <muh.ashlah@gmail.com>
2020-02-27 17:06:23 +0700

    add oauth handling on metadata service

--- metadata/main.go
+++ metadata/main.go
@@ -10,12 +10,43 @@ import (
     "./models"
 )
 
+func getOauthUser(token string) *string {
+    client := &http.Client{}
+
+    req, err := http.NewRequest("GET", os.Getenv("OAUTH_USER_RESOURCE"), nil)
+    req.Header.Add("Authorization", token)
+
+    resp, err := client.Do(req)
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    if resp.StatusCode == http.StatusUnauthorized {
+        return nil
+    }
+
+    var result map[string]interface{}
+    json.NewDecoder(resp.Body).Decode(&result)
+
+    userId := result["user_id"].(string)
+
+    return &userId
+}
+
 func uploadData(w http.ResponseWriter, r *http.Request) {
+    userPtr := getOauthUser(r.Header.Get("Authorization"))
+    if userPtr == nil {
+        http.Error(w, "Unauthorized", http.StatusUnauthorized)
+        return
+    }
+
     var fileData models.FileData
     err := json.NewDecoder(r.Body).Decode(&fileData)
     if err != nil {
         fmt.Println(err)
     }
+
+    fileData.Owner = *userPtr
 
     fmt.Println(fileData)
 
@@ -23,21 +54,27 @@ func uploadData(w http.ResponseWriter, r *http.Request) {
 }
 
 func files(w http.ResponseWriter, r *http.Request) {
+    userPtr := getOauthUser(r.Header.Get("Authorization"))
+    if userPtr == nil {
+        http.Error(w, "Unauthorized", http.StatusUnauthorized)
+        return
+    }
+
     dataId := r.URL.Query().Get("id")
 
     var fileData interface{}
     var err error
 
     if dataId == "" {
-         fileData, err = models.AllFileData("admin")
+         fileData, err = models.AllFileData(*userPtr)
     } else {
          dataId, _ := strconv.Atoi(dataId)
-         fileData, err = models.GetFileData(dataId, "admin")
+         fileData, err = models.GetFileData(dataId, *userPtr)
     }
 
     switch err {
          case sql.ErrNoRows:
-              fmt.Println("No rows")
+              fmt.Println("No matching data")
          case nil:
               jsonData, _ := json.Marshal(fileData)
               w.Header().Set("Content-Type", "application/json")
